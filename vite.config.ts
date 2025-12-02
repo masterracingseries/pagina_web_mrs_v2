a9f9165854687122bb70d1d4187e10e2cd111a4f
masterracingseries <gastonalexisep@gmail.com>
2025-12-02 20:04:13 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: './', // Esto asegura que la página cargue bien sea cual sea el nombre de tu repo
+export default defineConfig(({ mode }) => {
+  // Carga las variables de entorno (incluyendo las de GitHub Secrets)
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    base: './', // Esto asegura que la página cargue bien sea cual sea el nombre de tu repo en GitHub Pages
+    define: {
+      // AQUÍ OCURRE LA MAGIA:
+      // Reemplaza "process.env.API_KEY" en el código con el valor real de la variable VITE_API_KEY
+      'process.env.API_KEY': JSON.stringify(env.VITE_API_KEY),
+    }
+  }
 })
