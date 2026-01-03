78e99933928d112b4edb93ecef34ce3c1db2a89f
masterracingseries <gastonalexisep@gmail.com>
2026-01-03 00:07:37 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -12,6 +12,7 @@ import About from './components/About';
 import Footer from './components/Footer';
 import MiniGame from './components/MiniGame';
 import AIEngineer from './components/AIEngineer';
+import SandboxTools from './components/SandboxTools';
 
 const App: React.FC = () => {
   return (
@@ -30,6 +31,7 @@ const App: React.FC = () => {
         <About />
       </main>
       <Footer />
+      <SandboxTools />
     </div>
   );
 };
